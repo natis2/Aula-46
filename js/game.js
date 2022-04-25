e68fd277a3b5bb72ed892ec3743c180dbfac42e5
edwedja <edwedja_lima@hotmail.com>
2022-04-25 14:24:48 -0300

    first

--- js/game.js
+++ js/game.js
@@ -16,9 +16,31 @@ update(state) {
  start(){
    form=new Form ()
    form.display()
-   player = new Player();
-    playerCount = player.getCount();
 
+   player = new Player();
+   playerCount = player.getCount();
+
+   // criando sprites dos players
+   pl1 = createSprite(width / 2 - 200, height - 260);
+   pl1.addAnimation("p1", p1_img);
+   pl1.addAnimation("p1_running", p1_run);
+   pl1.scale = 3;
+
+   setTimeout(() => {
+     pl1.changeAnimation("p1_running", p1_run);
+   }, 5000);
+
+
+   pl2 = createSprite(width / 2 + 200, height - 245);
+   pl2.addAnimation("p2", p2_img);
+   pl2.addAnimation("p2_run", p2_run);
+   pl2.scale = 2.9;
+
+   setTimeout(() => {
+    pl2.changeAnimation("p2_run", p2_run);
+  }, 5000);
+
+   playerss = [pl1, pl2];
  }
  
 ocultar(){
@@ -27,7 +49,6 @@ ocultar(){
 
 play(){
   this.ocultar()
-  console.log(playerCount)
   if(playerCount===2){
     drawSprites()
     rect(width/2,height/2,100,100)
--- js/player.js
+++ js/player.js
@@ -21,15 +21,18 @@ class Player{
           
         });
       }
-      updateCount(count) {
-        database.ref("/").update({
-          playerCount: count
-        });
-      }
+
       getCount() {
         var playerCountRef = database.ref("playerCount");
         playerCountRef.on("value", data => {
           playerCount = data.val();
         });
       }
+      
+      updateCount(count) {
+        database.ref("/").update({
+          playerCount: count
+        });
+      }
+     
 }
